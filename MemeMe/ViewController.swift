fd575faf5da904359461f94e9f032b30db97e293
EyvindEmilio <eyvind.coaquira@gmail.com>
2022-03-24 01:14:03 -0400

    Updated font size

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "Impact", size: 32)!,
+        NSAttributedString.Key.font: UIFont(name: "Impact", size: 30)!,
         NSAttributedString.Key.strokeWidth: -4
     ]
     
